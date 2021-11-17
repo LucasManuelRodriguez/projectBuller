0312aa6c8640726017724ee719d031d73b664465
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-17 11:26:39 -0300

    modified root

--- scripts/date.js
+++ scripts/date.js
@@ -1,17 +1,17 @@
 export let picturesMobile =[
     {
         id: 1,    
-        src: '/assets/Fabrica_24 3.png',
+        src: './assets/Fabrica_24 3.png',
         alt: 'Hombre en trabajando en elaboracion de Cerveza'
     },
     {
         id: 2,    
-        src: '/assets/B05A5697 1.png',
+        src: './assets/B05A5697 1.png',
         alt: 'canillas de cerveza'
     },
     {
         id: 3,    
-        src: '/assets/B05A6167 1.png',
+        src: './assets/B05A6167 1.png',
         alt: 'cerveza'
     }
 ]
